eb6d92a08bd2fefdd1f38785b0acd100aa767c25
Olukayode <josepholukayode05@gmail.com>
2019-07-12 15:01:51 +0100

    created dashboard.dart

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DashBoard extends StatefulWidget {
+  @override
+  _DashBoardState createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome'),
+      ),
+    );
+  }
+}
