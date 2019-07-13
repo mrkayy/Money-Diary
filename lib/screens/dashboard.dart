e2d6575ca882effca5dc70f976c52c1bdbf0373a
Olukayode <josepholukayode05@gmail.com>
2019-07-13 15:00:41 +0100

    deleted branch

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,14 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class DashBoard extends StatefulWidget {
-  final FirebaseUser user;
-
-  const DashBoard({
-    Key key, 
-    @required this.user
-    }) : super(key: key);
-
   @override
   _DashBoardState createState() => _DashBoardState();
 }
@@ -17,24 +9,9 @@ class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            Icon(Icons.supervised_user_circle),
-          ],
-        ),
-      ),
       body: Center(
-        child: Text('Welcome ${widget.user.email}'),
+        child: Text('Welcome'),
       ),
     );
   }
-void navToSignUp(){
-
-}
-void navToSignIn(){
-  
-}
-
 }
